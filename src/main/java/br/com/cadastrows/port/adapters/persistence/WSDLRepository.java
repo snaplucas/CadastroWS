cb74710f4df4b61eea2360ab72ec4fe7c182c2a4
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-03-20 23:47:35 -0300

    update

---
+++ src/main/java/br/com/cadastrows/port/adapters/persistence/WSDLRepository.java
@@ -0,0 +1,6 @@
+package br.com.cadastrows.port.adapters.persistence;
+
+import br.com.cadastrows.domain.model.interfaces.IWSDLRepository;
+
+public class WSDLRepository implements IWSDLRepository {
+}
